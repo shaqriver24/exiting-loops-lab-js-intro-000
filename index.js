b2a7a87c785dcee8097115ecc30c8daeba350bed
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-29 01:21:29 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function breakOut(array, changeValue, stopValue) {
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] === stopValue) {
+      break;
+    }
+    else {
+      array[i] = changeValue;
+    }
+  }
+  return array;
+}
