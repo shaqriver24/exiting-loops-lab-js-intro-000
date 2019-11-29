5033b3a8de4289645618f3528dbe7bf9a747c8ac
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-29 01:49:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function keepGoing(array, changeValue, skipValue) {
 function findBy(array, findFn) {
   for(let i = 0; i < array.length; i++) {
     if(findFn(array[i])) {
-      return array[i];
+      return findFn();
     }
     return null;
   }
