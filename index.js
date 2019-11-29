fd59fc12e898b312fc625cc1617d5214ba77cc14
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-29 01:42:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,12 @@ function keepGoing(array, changeValue, skipValue) {
   }
   return array;
 }
+
+function findBy(array, findFn) {
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] === findFn) {
+      return array;
+    }
+    return null;
+  }
+}
